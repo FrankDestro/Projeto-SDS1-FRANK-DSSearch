750e488c2e55edfb50740693bf2511510462ab99
FrankDestro <frank-destro@outlook.com>
2022-08-17 19:56:21 -0300

    Security config

--- dssearch-backend/src/main/java/com/dev/dssearch/config/SecurityConfig.java
+++ dssearch-backend/src/main/java/com/dev/dssearch/config/SecurityConfig.java
@@ -23,15 +23,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private Environment env;
 
+	private static final String[] PUBLIC_MATCHERS = { "/**" };
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+
+		// H2
 		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
 			http.headers().frameOptions().disable();
 		}
 		
 		http.cors().and().csrf().disable();
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		http.authorizeRequests().anyRequest().permitAll();
+		http.authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll();
 	}
 
 	@Bean
