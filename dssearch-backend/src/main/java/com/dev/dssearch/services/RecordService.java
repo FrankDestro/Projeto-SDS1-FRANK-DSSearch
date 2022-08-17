4bdb59b42f035617bb8cc47b8291da045b7732a1
FrankDestro <frank-destro@outlook.com>
2022-08-17 18:23:53 -0300

    POST records

---
+++ dssearch-backend/src/main/java/com/dev/dssearch/services/RecordService.java
@@ -0,0 +1,40 @@
+package com.dev.dssearch.services;
+
+import java.time.Instant;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.dev.dssearch.config.dto.RecordDTO;
+import com.dev.dssearch.config.dto.RecordInsertDTO;
+import com.dev.dssearch.entities.Game;
+import com.dev.dssearch.entities.Record;
+import com.dev.dssearch.repositories.GameRepository;
+import com.dev.dssearch.repositories.RecordRepository;
+
+@Service
+public class RecordService {
+
+	@Autowired
+	private RecordRepository repository;
+
+	@Autowired
+	private GameRepository gameRepository;
+
+	@Transactional
+	public RecordDTO insert(RecordInsertDTO dto) {
+		
+		Record entity = new Record();
+		
+		entity.setName(dto.getName());
+		entity.setAge(dto.getAge());
+		entity.setMoment(Instant.now());
+
+		Game game = gameRepository.getOne(dto.getGameId());
+		entity.setGame(game);
+		
+		entity = repository.save(entity);
+		return new RecordDTO(entity);
+	}
+}
