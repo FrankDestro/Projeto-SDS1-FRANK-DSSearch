e43a50383f08e5959b664e6e3a566368b418578a
FrankDestro <frank-destro@outlook.com>
2022-08-17 15:58:47 -0300

    Entities, Repositories

---
+++ dssearch-backend/src/main/java/com/dev/dssearch/entities/enums/Platform.java
@@ -0,0 +1,7 @@
+package com.dev.dssearch.entities.enums;
+
+public enum Platform {
+	PC,
+	PLAYSTATION, 
+	XBOX
+}
---
+++ dssearch-backend/src/main/java/com/dev/dssearch/repositories/GameRepository.java
@@ -0,0 +1,11 @@
+package com.dev.dssearch.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.dev.dssearch.entities.Game;
+
+@Repository
+public interface GameRepository extends JpaRepository<Game, Long> {
+
+}
