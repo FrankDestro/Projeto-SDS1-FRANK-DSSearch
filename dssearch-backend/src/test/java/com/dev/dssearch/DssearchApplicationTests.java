88dec4e34ad8df7cf31aaef28769f794c4099661
FrankDestro <frank-destro@outlook.com>
2022-08-17 14:06:20 -0300

    Project created

---
+++ dssearch-backend/src/test/java/com/dev/dssearch/DssearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.dssearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DssearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
